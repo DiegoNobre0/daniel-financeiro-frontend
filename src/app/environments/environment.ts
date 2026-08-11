25b685f3d5c478ae65df6d9ce05f611550cb1583
Diego Nobre <diegonobre.dev@outlook.com>
2026-08-11 19:12:39 -0300

    fix: remove trailing slash from API URL in environment configuration

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,   
  //apiUrl: 'http://localhost:3333',
- apiUrl: 'https://api-daniel-financeiro-8dp8.vercel.app/',
+ apiUrl: 'https://api-daniel-financeiro-8dp8.vercel.app',
 };
