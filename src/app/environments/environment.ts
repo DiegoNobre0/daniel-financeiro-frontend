91ca44fadfb00684c241ae680dd41cb04f81377f
Diego Nobre <diegonobre.dev@outlook.com>
2026-08-11 19:09:15 -0300

    update environment configuration to use production API URL

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,   
- apiUrl: 'http://localhost:3333',
+ //apiUrl: 'http://localhost:3333',
+ apiUrl: 'https://api-daniel-financeiro-8dp8.vercel.app/',
 };
